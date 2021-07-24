9ea109e62b8e06e0f25f6f225d87032d5633a4e5
ashley-aguayo <ashley.aguayo@outlook.com>
2021-07-24 15:32:37 -0700

    update

--- static/js/map.js
+++ static/js/map.js
@@ -13,7 +13,7 @@ var myMap = L.map("map", {
 // Add a tile layer
 L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
     attribution: "© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>",
-    tileSize: 512,
+    tileSize: 300,
     maxZoom: 18,
     zoomOffset: -1,
     id: "mapbox/streets-v11",
