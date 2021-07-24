7e47f0c145bf2b791b1cb14eacc0d82ed369fb35
ashley-aguayo <ashley.aguayo@outlook.com>
2021-07-24 14:33:43 -0700

    updates

---
+++ static/js/config.js
@@ -0,0 +1,1 @@
+API_KEY ="pk.eyJ1IjoiYW5hZ3VheW84OSIsImEiOiJja3F1ODNhcmowMXp0MzFsMzh6dm54eTAxIn0.5ipJ_rkH0yV5m7gbdM3Abw"
---
+++ static/js/map.js
@@ -0,0 +1,30 @@
+//call json data
+// var url = "/api/crime.json";
+//     d3.json(url).then(function(response) {
+
+//     console.log(response);
+
+// Create a map object
+var myMap = L.map("map", {
+    center: [37.09, -95.71],
+    zoom: 5
+});
+
+// Add a tile layer
+L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
+    attribution: "© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>",
+    tileSize: 512,
+    maxZoom: 18,
+    zoomOffset: -1,
+    id: "mapbox/streets-v11",
+    accessToken: "pk.eyJ1IjoiYW5hZ3VheW84OSIsImEiOiJja3F1ODNhcmowMXp0MzFsMzh6dm54eTAxIn0.5ipJ_rkH0yV5m7gbdM3Abw"
+}).addTo(myMap);
+
+// var stateName = [];
+// var murderNumber = [];
+
+// function createMarkers (response) {
+//     var state = response.crimes.state
+//     console.log(state);
+// };
+
